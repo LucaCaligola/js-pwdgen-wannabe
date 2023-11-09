f315dc79a54d0f3a27440367666d49eb288f31bd
LucaCaligola <luca.caligola@gmail.com>
2023-11-09 16:04:57 +0100

    Password generator done!

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const nome = ( prompt ("Scrivi il tuo nome"));
+const cognome = ( prompt ("Scrivi il tuo cognome"));
+const colore = ( prompt ("Scrivi il tuo colore preferito"));
+
+const password = nome + cognome + colore;
+console.log (password);
+
+document.getElementById ('output').innerHTLM = 'La tua password è' + password;
